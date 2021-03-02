3487575b5fc14ef2181aff9e2566df40f4c0b404
Roberto_Cornacchiari <56387052+RobertoCornacchiari@users.noreply.github.com>
2021-03-02 13:39:30 +0100

    Fixed problems
    Fixed

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + "/public"));
+var clientDir = __dirname;
+for (let i = 0; i < 1; i++)
+    clientDir = clientDir.substring(0, clientDir.lastIndexOf("\\"));
+clientDir += "\\client";
+
+app.use(express.static(clientDir));
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 io.on("connection", (socketclient) => {
 	console.log("client connesso");
 	socketclient.emit("message", { message: "ciao", sender: "io me stesso" });
-	clients.push(socketclient);
 	socketclient.on("ping", () => {
 		console.log("ping from client");
 		socketclient.emit("pong", {});
